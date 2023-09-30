397e608395cb4ba7b12a250d2428d554c8fc56e1
Sabrina Estrada <estrasab@oregonstate.edu>
2023-09-30 01:01:01 -0700

    Implemented custom map function

--- application.js
+++ application.js
@@ -2,6 +2,16 @@
 VIP Application Submission
 Name: Sabrina Estrada
 OSU email: estrab@oregonstate.edu
-Description: Testing 
+Description: Custom map function implementation 
 */
 
+function mapFunc(func, arr) {
+    let newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        newArr[i] = func(arr[i]);
+    }
+    return newArr;
+}
+
+module.exports = mapFunc;
