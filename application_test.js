1f900e89d0e195cce733cb7b4eaace1e5d406e36
Sabrina Estrada <estrasab@oregonstate.edu>
2023-09-30 01:08:05 -0700

    Added two unit tests for map function

--- application_test.js
+++ application_test.js
@@ -5,3 +5,18 @@ OSU email: estrab@oregonstate.edu
 Description: Testing suite for mapFunc using Jest
 */
 
+const mapFunc = require('./application');
+
+// test to ensure that mapFunc squares the numbers in the array 
+test('mapFunc squares numbers correctly', () => {
+    const inputArr = [1, 2, 3, 4];
+    const expectedOutput = [1, 4, 9, 16];
+    expect(mapFunc(x => x * x, inputArr)).toStrictEqual(expectedOutput);
+});
+
+// test to ensure that mapFunc returns an empty array when passed an empty array
+test('mapFunc handles empty array correctly', () => {
+    const inputArr = [];
+    const expectedOutput = [];
+    expect(mapFunc(x => x * x, inputArr)).toStrictEqual(expectedOutput);
+});
