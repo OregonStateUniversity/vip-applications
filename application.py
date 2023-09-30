dc865fbc5a60fd9e2a51d83a53b76cd527b29ca3
Jonathan Hirsch <jonathan.hirsch2011@gmail.com>
2023-09-30 09:37:53 -0700

    added application.py and application_test.py

--- application.py
+++ application.py
@@ -1,4 +1,9 @@
 
-def custom_map(arr, func):
+def custom_map(arr, func) -> list:
+    """
+    Custom mapping function that will take any array and a given function, and 
+    apply that function to every item in the array. Uses dictionary comprehension 
+    to generate this new array. 
+    """
     new_arr = [func(x) for x in arr]
     return new_arr
--- application_test.py
+++ application_test.py
@@ -1,12 +1,18 @@
 from application import custom_map
 
 def test_custom_map1():
+    """
+    Tests and checks whether squaring numbers in the array is accurate.
+    """
     numbers = [1, 3, 5]
     squared_nums = custom_map(numbers, lambda x: x*x)
 
     assert squared_nums == [1, 9, 25]
 
 def test_custom_map2():
+    """
+    Tests and checks whether shifting letters by 1 is accurate.
+    """
     letters = ["a", "e", "g"]
 
     shifted_lets = custom_map(letters, lambda x: chr(ord(x)+1))
