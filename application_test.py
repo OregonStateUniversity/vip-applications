d8eb0e87cbff339848ad7b22313bab504378946e
Ryan Alexander Canete <caneter@oregonstate.edu>
2023-09-29 21:46:58 -0700

    Added test using unittest to compare created map function against python built in map function using n_squared function as a test

---
+++ application_test.py
@@ -0,0 +1,23 @@
+import unittest
+from application import n_squared, simple_map
+
+
+class TestSimpleMap(unittest.TestCase):
+    """
+    Is some unit tests that will test my implemented map 
+    function against pythons built in map function
+    """
+
+    def test_simple_map_n_squared(self):
+        """
+        Test of pythons map function against created map 
+        function using n_squared function
+        """
+        test_array = [2, 3, -100, 100, 0, 123, 43]
+        expected = list(map(n_squared, test_array))  # Convert map obj to list
+        result = simple_map(n_squared, test_array)
+        self.assertListEqual(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
