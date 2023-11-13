fcc43797e7b296a16f67422d1fe05a10b9d0f2c2
Derrick <macarand@oregonstate.edu>
2023-11-13 00:19:50 -0800

    Added new test

--- application_test.js
+++ application_test.js
@@ -15,3 +15,11 @@ test('propMapper extracts names from objects in an array', () => {
   expect(propMapper(getProp, arr, 'name')).toEqual(result)
 })
 
+test('propMapper extracts ages from objects in an array', () => {
+  const arr = [{ name: 'Grace', age: 25 }, { name: 'Kevin', age: 30 }, { name: 'John', age: 21 }]
+  const result = [25, 30, 21]
+
+  const getProp = (obj, prop) => obj[prop]
+
+  expect(propMapper(getProp, arr, 'age')).toEqual(result)
+})
