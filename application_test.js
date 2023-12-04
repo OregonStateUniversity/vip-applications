50c60e5e83adbb2602e43c2ffba92ad0e1b8fa9f
sycamedia <parkerdennis413@gmail.com>
2023-12-03 22:44:25 -0500

    Define unit test

--- application_test.js
+++ application_test.js
@@ -1,6 +1,17 @@
 // Author: sycamedia
 // Date: 3 December 2023
-// Description: For VIP Application. Unit test for application.js.
+// Description: For VIP Application. Unit test for application.js using Jest.
 
 // Used this as a guide: https://www.freecodecamp.org/news/how-to-start-unit-testing-javascript/
 
+const map = require("./application");
+
+testArr = [2, -5, 10, 100]
+
+function testFunc(x) {
+    return x + 2
+}
+
+test("Run map function", () => {
+expect(map(testFunc, testArr)).toStrictEqual([4, -3, 12, 102]);
+});
