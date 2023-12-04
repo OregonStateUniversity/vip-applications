c9d24bc4328be4e130d55479444c35180e0a96f4
sycamedia <parkerdennis413@gmail.com>
2023-12-03 22:25:49 -0500

    Add application_test.js to repo

---
+++ application_test.js
@@ -0,0 +1,6 @@
+// Author: sycamedia
+// Date: 3 December 2023
+// Description: For VIP Application. Unit test for application.js.
+
+// Used this as a guide: https://www.freecodecamp.org/news/how-to-start-unit-testing-javascript/
+
