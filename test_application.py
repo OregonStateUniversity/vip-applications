416cfd173a0bf1478205ecada9837a7b5e6ff4ee
Paola Cernada <96987381+paolacernada@users.noreply.github.com>
2024-01-30 03:46:14 -0500

    Updated test_application.py

--- test_application.py
+++ test_application.py
@@ -5,8 +5,8 @@ from application import map_func
 class TestMapFunction(unittest.TestCase):
 
     def test_map_function(self):
-        input_list = [1, 2, 3]
-        expected = [2, 4, 6]
+        input_list = [4, 7, 18]
+        expected = [8, 14, 36]
         result = map_func(lambda x: x * 2, input_list)
         self.assertEqual(result, expected)
 
