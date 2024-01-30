36bc5401ec7e669691e032b3680fdc4aca48931e
Luis Sosa <94214924+LuisJSosa@users.noreply.github.com>
2024-01-29 23:31:15 -0500

    added main logic

--- application.go
+++ application.go
@@ -1,5 +1,6 @@
 package main
 
+// Map Transform each element of input_slice
 func Map[input_type any, output_type any](
 	input_slice []input_type,
 	trasnformation_func func(input_type) output_type,
@@ -7,5 +8,9 @@ func Map[input_type any, output_type any](
 
 	result_slice := make([]output_type, len(input_slice))
 
+	// Apply transformation_func to each input_slice element
+	for index, value := range input_slice {
+		result_slice[index] = trasnformation_func(value)
+	}
 	return result_slice
 }
